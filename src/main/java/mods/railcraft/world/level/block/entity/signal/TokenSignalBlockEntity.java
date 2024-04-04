2d3b0c3b21169e6a280594fc4b0441434c8e7b49
enn3 <arturpeshko39@gmail.com>
2024-04-04 18:11:27 +0200

    now token signals support up to 10km blocks

--- src/main/java/mods/railcraft/world/level/block/entity/signal/TokenSignalBlockEntity.java
+++ src/main/java/mods/railcraft/world/level/block/entity/signal/TokenSignalBlockEntity.java
@@ -2,6 +2,7 @@ package mods.railcraft.world.level.block.entity.signal;
 
 import java.util.Objects;
 import java.util.UUID;
+
 import mods.railcraft.api.signal.SignalAspect;
 import mods.railcraft.api.signal.SimpleSignalController;
 import mods.railcraft.api.signal.TokenSignalEntity;
@@ -21,138 +22,137 @@ import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class TokenSignalBlockEntity extends AbstractSignalBlockEntity
-    implements SignalControllerEntity, TokenSignalEntity {
+        implements SignalControllerEntity, TokenSignalEntity {
 
-  private final SimpleSignalController signalController =
-      new SimpleSignalController(1, this::syncToClient, this, false,
-          __ -> this.level.getLightEngine().checkBlock(this.getBlockPos()));
+    private final SimpleSignalController signalController =
+            new SimpleSignalController(1, this::syncToClient, this, false,
+                    __ -> this.level.getLightEngine().checkBlock(this.getBlockPos()));
 
-  private UUID ringId = UUID.randomUUID();
-  private BlockPos ringCentroidPos;
+    private UUID ringId = UUID.randomUUID();
+    private BlockPos ringCentroidPos;
 
-  private final TimerBag<UUID> cartTimers = new TimerBag<>(8);
-  private final TrackLocator trackLocator;
+    private final TimerBag<UUID> cartTimers = new TimerBag<>(8);
+    private final TrackLocator trackLocator;
 
-  public TokenSignalBlockEntity(BlockPos blockPos, BlockState blockState) {
-    this(RailcraftBlockEntityTypes.TOKEN_SIGNAL.get(), blockPos, blockState);
-  }
-
-  public TokenSignalBlockEntity(BlockEntityType<?> type, BlockPos blockPos, BlockState blockState) {
-    super(type, blockPos, blockState);
-    this.trackLocator = new TrackLocator(this::getLevel, blockPos);
-  }
-
-  @Override
-  public void onLoad() {
-    super.onLoad();
-    if (!this.level.isClientSide()) {
-      this.signalController.refresh();
-    }
-  }
-
-  public void blockRemoved() {
-    this.signalController.destroy();
-    if (!this.level.isClientSide()) {
-      this.signalNetwork().removePeer(this.getBlockPos());
-    }
-  }
-
-  public static void clientTick(Level level, BlockPos blockPos, BlockState blockState,
-      TokenSignalBlockEntity blockEntity) {
-    blockEntity.signalController.spawnTuningAuraParticles();
-  }
-
-  public static void serverTick(Level level, BlockPos blockPos, BlockState blockState,
-      TokenSignalBlockEntity blockEntity) {
-
-    var tokenRing = blockEntity.signalNetwork();
-    if (!Objects.equals(blockEntity.ringCentroidPos, tokenRing.getCentroid())) {
-      blockEntity.ringCentroidPos = tokenRing.getCentroid();
-      blockEntity.syncToClient();
+    public TokenSignalBlockEntity(BlockPos blockPos, BlockState blockState) {
+        this(RailcraftBlockEntityTypes.TOKEN_SIGNAL.get(), blockPos, blockState);
     }
 
-    blockEntity.cartTimers.tick();
-    if (blockEntity.trackLocator.trackStatus() == TrackLocator.Status.VALID) {
-      var trackPos = blockEntity.trackLocator.trackPos();
-      if (trackPos != null) {
-        EntitySearcher.findMinecarts()
-            .at(trackPos)
-            .stream(level)
-            .filter(cart -> blockEntity.cartTimers.add(cart.getUUID()))
-            .forEach(tokenRing::markCart);
-      }
+    public TokenSignalBlockEntity(BlockEntityType<?> type, BlockPos blockPos, BlockState blockState) {
+        super(type, blockPos, blockState);
+        this.trackLocator = new TrackLocator(this::getLevel, blockPos);
     }
 
-    blockEntity.signalController.setSignalAspect(blockEntity.signalNetwork().aspect());
-  }
-
-  @Override
-  public SignalAspect getPrimarySignalAspect() {
-    return this.signalController.aspect();
-  }
-
-  @Override
-  protected void saveAdditional(CompoundTag tag) {
-    super.saveAdditional(tag);
-    tag.put("network", this.signalController.serializeNBT());
-    tag.putUUID("tokenRingId", this.ringId);
-  }
-
-  @Override
-  public void load(CompoundTag tag) {
-    super.load(tag);
-    this.signalController.deserializeNBT(tag.getCompound("network"));
-    this.ringId = tag.getUUID("tokenRingId");
-  }
-
-  @Override
-  public void writeToBuf(FriendlyByteBuf data) {
-    super.writeToBuf(data);
-    this.signalController.writeToBuf(data);
-    data.writeBlockPos(this.signalNetwork().getCentroid());
-    data.writeUUID(this.ringId);
-  }
-
-  @Override
-  public void readFromBuf(FriendlyByteBuf data) {
-    super.readFromBuf(data);
-    this.signalController.readFromBuf(data);
-    this.ringCentroidPos = data.readBlockPos();
-    this.ringId = data.readUUID();
-  }
-
-  @Override
-  public UUID ringId() {
-    return this.ringId;
-  }
-
-  public void setRingId(UUID tokenRingId) {
-    this.ringId = tokenRingId;
-  }
-
-  @Override
-  public BlockPos ringCentroidPos() {
-    if (this.ringCentroidPos == null)
-      return this.getBlockPos();
-    return this.ringCentroidPos;
-  }
-
-  @Override
-  public SimpleSignalController getSignalController() {
-    return this.signalController;
-  }
-
-  @Override
-  public SimpleTokenRing signalNetwork() {
-    if (this.level.isClientSide()) {
-      throw new IllegalStateException("Token ring is not available on the client.");
+    @Override
+    public void onLoad() {
+        super.onLoad();
+        if (!this.level.isClientSide()) {
+            this.signalController.refresh();
+        }
     }
-    return TokenRingManager.get((ServerLevel) this.level)
-        .getTokenRingNetwork(this.ringId, this.getBlockPos());
-  }
 
-  @Override
-  public TrackLocator trackLocator() {
-    return this.trackLocator;
-  }
+    public void blockRemoved() {
+        this.signalController.destroy();
+        if (!this.level.isClientSide()) {
+            this.signalNetwork().removePeer(this.getBlockPos());
+        }
+    }
+
+    public static void clientTick(Level level, BlockPos blockPos, BlockState blockState,
+                                  TokenSignalBlockEntity blockEntity) {
+        blockEntity.signalController.spawnTuningAuraParticles();
+    }
+
+    public static void serverTick(Level level, BlockPos blockPos, BlockState blockState,
+                                  TokenSignalBlockEntity blockEntity) {
+        var tokenRing = blockEntity.signalNetwork();
+        if (!Objects.equals(blockEntity.ringCentroidPos, tokenRing.getCentroid())) {
+            blockEntity.ringCentroidPos = tokenRing.getCentroid();
+            blockEntity.syncToClient();
+        }
+
+        blockEntity.cartTimers.tick();
+        if (blockEntity.trackLocator.trackStatus() == TrackLocator.Status.VALID) {
+            var trackPos = blockEntity.trackLocator.trackPos();
+            if (trackPos != null) {
+                EntitySearcher.findMinecarts()
+                        .at(trackPos)
+                        .stream(level)
+                        .filter(cart -> blockEntity.cartTimers.add(cart.getUUID()))
+                        .forEach(tokenRing::markCart);
+            }
+        }
+
+        blockEntity.signalController.setSignalAspect(blockEntity.signalNetwork().aspect());
+    }
+
+    @Override
+    public SignalAspect getPrimarySignalAspect() {
+        return this.signalController.aspect();
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag tag) {
+        super.saveAdditional(tag);
+        tag.put("network", this.signalController.serializeNBT());
+        tag.putUUID("tokenRingId", this.ringId);
+    }
+
+    @Override
+    public void load(CompoundTag tag) {
+        super.load(tag);
+        this.signalController.deserializeNBT(tag.getCompound("network"));
+        this.ringId = tag.getUUID("tokenRingId");
+    }
+
+    @Override
+    public void writeToBuf(FriendlyByteBuf data) {
+        super.writeToBuf(data);
+        this.signalController.writeToBuf(data);
+        data.writeBlockPos(this.signalNetwork().getCentroid());
+        data.writeUUID(this.ringId);
+    }
+
+    @Override
+    public void readFromBuf(FriendlyByteBuf data) {
+        super.readFromBuf(data);
+        this.signalController.readFromBuf(data);
+        this.ringCentroidPos = data.readBlockPos();
+        this.ringId = data.readUUID();
+    }
+
+    @Override
+    public UUID ringId() {
+        return this.ringId;
+    }
+
+    public void setRingId(UUID tokenRingId) {
+        this.ringId = tokenRingId;
+    }
+
+    @Override
+    public BlockPos ringCentroidPos() {
+        if (this.ringCentroidPos == null)
+            return this.getBlockPos();
+        return this.ringCentroidPos;
+    }
+
+    @Override
+    public SimpleSignalController getSignalController() {
+        return this.signalController;
+    }
+
+    @Override
+    public SimpleTokenRing signalNetwork() {
+        if (this.level.isClientSide()) {
+            throw new IllegalStateException("Token ring is not available on the client.");
+        }
+        return TokenRingManager.get((ServerLevel) this.level)
+                .getTokenRingNetwork(this.ringId, this.getBlockPos());
+    }
+
+    @Override
+    public TrackLocator trackLocator() {
+        return this.trackLocator;
+    }
 }
--- src/main/java/mods/railcraft/world/signal/SimpleTokenRing.java
+++ src/main/java/mods/railcraft/world/signal/SimpleTokenRing.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
+
 import mods.railcraft.api.signal.SignalAspect;
 import mods.railcraft.api.signal.TokenRing;
 import mods.railcraft.api.signal.TokenSignalEntity;
@@ -24,164 +25,166 @@ import net.minecraft.world.entity.vehicle.AbstractMinecart;
  */
 public class SimpleTokenRing implements TokenRing {
 
-  private static final int MAX_DISTANCE = 256 * 256;
-  private final ServerLevel level;
-  private final TokenRingManager manager;
-  private final UUID id;
-  private final Set<BlockPos> peers = new HashSet<>();
-  private final Set<UUID> trackedCarts = new HashSet<>();
-  private BlockPos centroid = BlockPos.ZERO;
-  private boolean linking;
+    private static final int MAX_DISTANCE = 10000 * 10000;
+    private final ServerLevel level;
+    private final TokenRingManager manager;
+    private final UUID id;
+    private final Set<BlockPos> peers = new HashSet<>();
+    private final Set<UUID> trackedCarts = new HashSet<>();
+    private BlockPos centroid = BlockPos.ZERO;
+    private boolean linking;
 
-  public SimpleTokenRing(ServerLevel level, TokenRingManager manager, UUID id) {
-    this.level = level;
-    this.manager = manager;
-    this.id = id;
-  }
-
-  public SimpleTokenRing(ServerLevel level, TokenRingManager manager, UUID id, BlockPos origin) {
-    this(level, manager, id);
-    this.addSignal(origin);
-  }
-
-  @Override
-  public boolean isLinking() {
-    return this.linking;
-  }
-
-  @Override
-  public void startLinking() {
-    this.linking = true;
-  }
-
-  @Override
-  public void stopLinking() {
-    this.linking = false;
-  }
-
-  @Override
-  public boolean addPeer(TokenSignalEntity peer) {
-    BlockPos otherPos = peer.asBlockEntity().getBlockPos();
-
-    if (this.peers.stream().anyMatch(pos -> pos.distSqr(otherPos) > MAX_DISTANCE)) {
-      return false;
+    public SimpleTokenRing(ServerLevel level, TokenRingManager manager, UUID id) {
+        this.level = level;
+        this.manager = manager;
+        this.id = id;
     }
-    TokenRing otherRing = peer.signalNetwork();
-    if (otherRing != this) {
-      otherRing.removePeer(peer.asBlockEntity().getBlockPos());
+
+    public SimpleTokenRing(ServerLevel level, TokenRingManager manager, UUID id, BlockPos origin) {
+        this(level, manager, id);
+        this.addSignal(origin);
     }
-    // TokenRing tokenRing = otherRing.signals.size() > signals.size() ? otherRing : this;
-    SimpleTokenRing tokenRing = this;
-    peer.setRingId(tokenRing.getId());
-    tokenRing.addSignal(peer.asBlockEntity().getBlockPos());
-    return true;
-  }
 
-  public void tick() {
-    if (!this.peers.isEmpty()) {
-      BlockPos origin = this.peers.stream().findAny().orElse(BlockPos.ZERO);
-      BoxBuilder aabbFactory = BoxBuilder.create().at(origin);
-      for (BlockPos pos : this.peers) {
-        aabbFactory.expandToCoordinate(pos);
-      }
-      aabbFactory.inflate(16).clampToWorld();
-      this.trackedCarts.retainAll(
-          EntitySearcher.findMinecarts()
-              .in(aabbFactory.build())
-              .stream(this.level)
-              .map(Entity::getUUID)
-              .collect(Collectors.toSet()));
+    @Override
+    public boolean isLinking() {
+        return this.linking;
     }
-  }
 
-  public boolean isOrphaned(ServerLevel level) {
-    return !this.peers.stream().map(this::peerAt).allMatch(Optional::isPresent);
-  }
-
-  void loadSignals(Collection<BlockPos> signals) {
-    this.peers.addAll(signals);
-    this.centroid = MathUtil.centroid(signals);
-  }
-
-  void loadCarts(Collection<UUID> carts) {
-    this.trackedCarts.addAll(carts);
-  }
-
-  public boolean addSignal(BlockPos pos) {
-    boolean changed = this.peers.add(pos);
-    if (changed)
-      this.signalsChanged();
-    return changed;
-  }
-
-  private void signalsChanged() {
-    this.manager.setDirty();
-    this.centroid = MathUtil.centroid(this.peers);
-  }
-
-  public void markCart(AbstractMinecart cart) {
-    UUID cartID = cart.getUUID();
-    if (this.trackedCarts.remove(cartID)) {
-      this.manager.setDirty();
-      return;
+    @Override
+    public void startLinking() {
+        this.linking = true;
     }
-    if (this.trackedCarts.add(cartID))
-      this.manager.setDirty();
-  }
 
-  @Override
-  public Collection<BlockPos> peers() {
-    return Collections.unmodifiableSet(this.peers);
-  }
-
-  @Override
-  public Set<UUID> getTrackedCarts() {
-    return Collections.unmodifiableSet(this.trackedCarts);
-  }
-
-  @Override
-  public SignalAspect aspect() {
-    if (this.isLinking()) {
-      return SignalAspect.BLINK_YELLOW;
-    } else if (this.peers.size() <= 1) {
-      return SignalAspect.BLINK_RED;
+    @Override
+    public void stopLinking() {
+        this.linking = false;
     }
-    return this.trackedCarts.isEmpty() ? SignalAspect.GREEN : SignalAspect.RED;
-  }
 
-  @Override
-  public UUID getId() {
-    return this.id;
-  }
+    @Override
+    public boolean addPeer(TokenSignalEntity peer) {
+        BlockPos otherPos = peer.asBlockEntity().getBlockPos();
 
-  @Override
-  public BlockPos getCentroid() {
-    return this.centroid;
-  }
-
-  @Override
-  public Optional<TokenSignalEntity> peerAt(BlockPos blockPos) {
-    if (!this.level.isLoaded(blockPos)) {
-      return Optional.empty();
+        if (this.peers.stream().anyMatch(pos -> pos.distSqr(otherPos) > MAX_DISTANCE)) {
+            return false;
+        }
+        TokenRing otherRing = peer.signalNetwork();
+        if (otherRing != this) {
+            otherRing.removePeer(peer.asBlockEntity().getBlockPos());
+        }
+        // TokenRing tokenRing = otherRing.signals.size() > signals.size() ? otherRing : this;
+        SimpleTokenRing tokenRing = this;
+        peer.setRingId(tokenRing.getId());
+        tokenRing.addSignal(peer.asBlockEntity().getBlockPos());
+        return true;
     }
-    var blockEntity = this.level.getBlockEntity(blockPos);
-    return blockEntity instanceof TokenSignalBlockEntity tokenSignal && !blockEntity.isRemoved()
-        ? Optional.of(tokenSignal)
-        : Optional.empty();
-  }
 
-  @Override
-  public boolean removePeer(BlockPos peerPos) {
-    if (this.peers.remove(peerPos)) {
-      this.signalsChanged();
-      return true;
+    public void tick() {
+        if (!this.peers.isEmpty()) {
+            BlockPos origin = this.peers.stream().findAny().orElse(BlockPos.ZERO);
+            BoxBuilder aabbFactory = BoxBuilder.create().at(origin);
+            for (BlockPos pos : this.peers) {
+                aabbFactory.expandToCoordinate(pos);
+            }
+            aabbFactory.inflate(16).clampToWorld();
+            this.trackedCarts.retainAll(
+                    EntitySearcher.findMinecarts()
+                            .in(aabbFactory.build())
+                            .stream(this.level)
+                            .map(Entity::getUUID)
+                            .collect(Collectors.toSet()));
+        }
     }
-    return false;
-  }
 
-  @Override
-  public void refresh() {}
+    public boolean isOrphaned(ServerLevel level) {
+        return !this.peers.stream().map(this::peerAt).allMatch(Optional::isPresent);
+    }
 
-  @Override
-  public void destroy() {}
+    void loadSignals(Collection<BlockPos> signals) {
+        this.peers.addAll(signals);
+        this.centroid = MathUtil.centroid(signals);
+    }
+
+    void loadCarts(Collection<UUID> carts) {
+        this.trackedCarts.addAll(carts);
+    }
+
+    public boolean addSignal(BlockPos pos) {
+        boolean changed = this.peers.add(pos);
+        if (changed)
+            this.signalsChanged();
+        return changed;
+    }
+
+    private void signalsChanged() {
+        this.manager.setDirty();
+        this.centroid = MathUtil.centroid(this.peers);
+    }
+
+    public void markCart(AbstractMinecart cart) {
+        UUID cartID = cart.getUUID();
+        if (this.trackedCarts.remove(cartID)) {
+            this.manager.setDirty();
+            return;
+        }
+        if (this.trackedCarts.add(cartID))
+            this.manager.setDirty();
+    }
+
+    @Override
+    public Collection<BlockPos> peers() {
+        return Collections.unmodifiableSet(this.peers);
+    }
+
+    @Override
+    public Set<UUID> getTrackedCarts() {
+        return Collections.unmodifiableSet(this.trackedCarts);
+    }
+
+    @Override
+    public SignalAspect aspect() {
+        if (this.isLinking()) {
+            return SignalAspect.BLINK_YELLOW;
+        } else if (this.peers.size() <= 1) {
+            return SignalAspect.BLINK_RED;
+        }
+        return this.trackedCarts.isEmpty() ? SignalAspect.GREEN : SignalAspect.RED;
+    }
+
+    @Override
+    public UUID getId() {
+        return this.id;
+    }
+
+    @Override
+    public BlockPos getCentroid() {
+        return this.centroid;
+    }
+
+    @Override
+    public Optional<TokenSignalEntity> peerAt(BlockPos blockPos) {
+        if (!this.level.isLoaded(blockPos)) {
+            return Optional.empty();
+        }
+        var blockEntity = this.level.getBlockEntity(blockPos);
+        return blockEntity instanceof TokenSignalBlockEntity tokenSignal && !blockEntity.isRemoved()
+                ? Optional.of(tokenSignal)
+                : Optional.empty();
+    }
+
+    @Override
+    public boolean removePeer(BlockPos peerPos) {
+        if (this.peers.remove(peerPos)) {
+            this.signalsChanged();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void refresh() {
+    }
+
+    @Override
+    public void destroy() {
+    }
 }
